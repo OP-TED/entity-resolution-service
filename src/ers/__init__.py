13a8f29e35efd170c2c7f22486e829a82710ecc2
Meaningfy <hi@meaningfy.ws>
2026-03-19 13:25:34 +0200

    feat: add config for ERS rest api

--- src/ers/__init__.py
+++ src/ers/__init__.py
@@ -79,6 +79,20 @@ class RDFMentionParserConfig:
         return config_value
 
 
+class ERSRestApiConfig:
+    @env_property(default_value="ERS REST API")
+    def ERS_API_NAME(self, config_value: str) -> str:
+        return config_value
+
+    @env_property(default_value="/api/v1")
+    def ERS_API_PREFIX(self, config_value: str) -> str:
+        return config_value
+
+    @env_property(default_value="8001")
+    def ERS_API_PORT(self, config_value: str) -> int:
+        return int(config_value)
+
+
 class ERSConfigResolver(
     AppConfig,
     JWTConfig,
@@ -86,6 +100,7 @@ class ERSConfigResolver(
     CurationConfig,
     MongoDBConfig,
     RDFMentionParserConfig,
+    ERSRestApiConfig,
 ):
     """Aggregates all ERS configuration.
 
